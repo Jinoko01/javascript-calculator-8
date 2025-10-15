25bfef93c7835169c4afe2faf6a2d5ddb30a5341
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 15:05:21 +0900

    test(delimiterSetter): Add delimiterSetter error case
    - Only one letter is possible
    - Can't use numbers
    - Can't use space
    - Should be set up correctly

--- __tests__/DelimiterSetterTest.js
+++ __tests__/DelimiterSetterTest.js
@@ -10,8 +10,71 @@ describe('DelimiterSetter 테스트', () => {
     ];
 
     const delimiterSetter = new DelimiterSetter();
+
     inputs.forEach((input, index) => {
       expect(delimiterSetter.setDelimiter(input)).toEqual(returns[index]);
+    });
+  });
+});
+
+describe('DelimiterSetter 에러 테스트', () => {
+  it('기본 구분자는 커스텀 구분자로 설정할 수 없다.', () => {
+    const inputs = ['//,\n1,2,3', '//:\n1:2:3'];
+
+    const delimiterSetter = new DelimiterSetter();
+
+    inputs.forEach((input) => {
+      expect(() => delimiterSetter.setDelimiter(input)).toThrow();
+    });
+  });
+
+  it('커스텀 구분자는 1글자만 가능하다.', () => {
+    const inputs = ['//!!\n1!!2!!3', '//!@#!@\n1!@#!@2!@#!@3'];
+
+    const delimiterSetter = new DelimiterSetter();
+
+    inputs.forEach((input) => {
+      expect(() => delimiterSetter.setDelimiter(input)).toThrow();
+    });
+  });
+
+  it('커스텀 구분자로 숫자를 사용할 수 없다.', () => {
+    const inputs = ['//1\n11213', '//0\n10203', '//100\n110021003'];
+
+    const delimiterSetter = new DelimiterSetter();
+
+    inputs.forEach((input) => {
+      expect(() => delimiterSetter.setDelimiter(input)).toThrow();
+    });
+  });
+
+  it('커스텀 구분자로 빈 문자열 혹은 공백을 사용하면 안된다.', () => {
+    const inputs = ['//\n123', '// \n1@2@3', '//\n\n1\n2\n3', '//\t\n1\t2\t3'];
+
+    const delimiterSetter = new DelimiterSetter();
+
+    inputs.forEach((input) => {
+      expect(() => delimiterSetter.setDelimiter(input)).toThrow();
+    });
+  });
+
+  it('커스텀 구분자를 올바르게 설정해야 한다.', () => {
+    const inputs = [
+      '/;\n1;2;3',
+      ';\n1;2;3',
+      '///;\n1;2;3',
+      '//;m1;2;3',
+      '//;n1;2;3',
+      '//;\\1;2;3',
+      '//;1;2;3',
+      ';1;2;3',
+      '//;\\n1;2;3',
+    ];
+
+    const delimiterSetter = new DelimiterSetter();
+
+    inputs.forEach((input) => {
+      expect(delimiterSetter.setDelimiter(input)).toThrow();
     });
   });
 });
