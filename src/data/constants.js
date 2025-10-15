d5bc2232fbbbfbb8d1e965a2b5207c3a6c8b977d
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 14:11:03 +0900

    feat(constants): Add input error message
    invalid type and input empty string

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,3 +1,9 @@
 const SYSTEM_MESSAGE = Object.freeze({
   INPUT: '덧셈할 문자열을 입력해 주세요.\n',
 });
+
+const ERROR_PREFIX = '[ERROR] ';
+const ERROR_MESSAGE = Object.freeze({
+  INVALID_TYPE: `${ERROR_PREFIX}문자열이 아닌 입력값이 있습니다.`,
+  INPUT_EMPTY_STRING: `${ERROR_PREFIX}입력값이 올바르지 않습니다.`,
+});
