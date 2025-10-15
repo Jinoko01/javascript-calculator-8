57b9b9e182429977210edfca9a0d44298f867567
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 16:38:04 +0900

    feat(calculator): Add calculate method

---
+++ src/entities/Calculator.js
@@ -0,0 +1,21 @@
+import AddOperator from './AddOperator.js';
+import Parser from './Parser.js';
+
+export default class Calculator {
+  constructor() {
+    this.parser = new Parser();
+    this.addOperator = new AddOperator();
+  }
+
+  calculate(input) {
+    const arr = this.parse(input);
+  }
+
+  parse(input) {
+    return this.parser.parse(input);
+  }
+
+  add(nums) {
+    return this.addOperator.add(nums);
+  }
+}
