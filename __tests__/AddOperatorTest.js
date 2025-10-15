f15eb0125aeed59f5e1739d6052ccdcabe7652ec
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 16:31:21 +0900

    fix(addOperator): Fix returns value

--- __tests__/AddOperatorTest.js
+++ __tests__/AddOperatorTest.js
@@ -3,7 +3,7 @@ import AddOperator from '../src/entities/AddOperator.js';
 describe('AddOperator 테스트', () => {
   it('덧셈 연산을 수행한다.', () => {
     const inputs = [[1, 2, 3], [100, 200, 300], [5]];
-    const returns = [6, 600];
+    const returns = [6, 600, 5];
 
     const addOperator = new AddOperator();
 
