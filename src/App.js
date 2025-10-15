d1878037a73a7e78be3adebf00a6f828e8feae52
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 23:40:46 +0900

    fix(app): Fix readLineAsync

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
-import InputProcessor from './entities/InputProcessor';
+import InputProcessor from './entities/InputProcessor.js';
+import { SYSTEM_MESSAGE } from './data/messages.js';
 
 class App {
   async run() {
-    const input = await Console.readLineAsync();
+    const input = await Console.readLineAsync(SYSTEM_MESSAGE.INPUT);
 
     const inputProcessor = new InputProcessor();
     const result = inputProcessor.processInput(input);
