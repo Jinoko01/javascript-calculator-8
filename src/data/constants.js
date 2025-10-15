5be3f3f21482de8b5cc26ed87bd37f6cc58f2746
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 15:07:47 +0900

    fix(constants): Fix export setting

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,9 +1,12 @@
-const SYSTEM_MESSAGE = Object.freeze({
+export const SYSTEM_MESSAGE = Object.freeze({
   INPUT: '덧셈할 문자열을 입력해 주세요.\n',
 });
 
 const ERROR_PREFIX = '[ERROR] ';
-const ERROR_MESSAGE = Object.freeze({
+export const ERROR_MESSAGE = Object.freeze({
   INVALID_TYPE: `${ERROR_PREFIX}문자열이 아닌 입력값이 있습니다.`,
   INPUT_EMPTY_STRING: `${ERROR_PREFIX}입력값이 올바르지 않습니다.`,
+  INVALID_CUSTOM_DELIMITER_SETTING: `${ERROR_PREFIX}커스텀 구분자 설정이 잘못되었습니다.`,
+  INVALID_CUSTOM_DELIMITER_SETTING_LENGTH: `${ERROR_PREFIX}커스텀 구분자는 1글자만 가능합니다.`,
+  INVALID_CUSTOM_DELIMITER_SETTING_CONTENT: `${ERROR_PREFIX}커스텀 구분자는 숫자나 공백을 사용할 수 없습니다.`,
 });
