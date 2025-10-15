a33bf7c6e9799ac2580931f4d336da2cb8fc55b7
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 16:47:40 +0900

    feat(app): Feature calculator application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
+import InputProcessor from './entities/InputProcessor';
 
 class App {
   async run() {
-    const input = await Console.readlineAsync();
+    const input = await Console.readLineAsync();
+
+    const inputProcessor = new InputProcessor();
+    const result = inputProcessor.processInput(input);
+
+    Console.print(result);
   }
 }
 
