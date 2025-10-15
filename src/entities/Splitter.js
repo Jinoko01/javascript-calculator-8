c1af979721305d7f294a2958d8943494a8099cca
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 23:40:18 +0900

    fix(splitter): Add replace special character to escape string

--- src/entities/Splitter.js
+++ src/entities/Splitter.js
@@ -1,6 +1,9 @@
 export default class Splitter {
   split(input, delimiter) {
-    const delimiterRegex = new RegExp(`[${delimiter.join('')}]`, 'g');
+    const escaped = delimiter
+      .map((s) => s.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&'))
+      .sort((a, b) => b.length - a.length);
+    const delimiterRegex = new RegExp(`(?:${escaped.join('|')})`, 'g');
     return input.split(delimiterRegex).map(Number);
   }
 }
