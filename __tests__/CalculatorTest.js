16bf0dba9a18e10af3252d8c6e7d4328e53d62b1
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 14:35:57 +0900

    test(calculator): Add calculator test case
    Return an array of separated numbers based on the delimiter

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -12,4 +12,28 @@ describe('Calculator 테스트', () => {
       expect(process).toHaveBeenCalledWith(input);
     });
   });
+
+  it('parse 메소드는 구분자를 기준으로 나뉘어진 숫자 배열을 반환한다.', () => {
+    const inputs = [
+      '1,2,3',
+      '//;\n11;12;13',
+      '100:200,300',
+      '//@\n1,22:333@4444',
+    ];
+    const outputs = [
+      [1, 2, 3],
+      [11, 12, 13],
+      [100, 200, 300],
+      [1, 22, 333, 4444],
+    ];
+
+    const calculator = new Calculator();
+    const process = jest.spyOn(calculator, 'parse');
+
+    inputs.forEach((input) => {
+      const result = calculator.parse(input);
+      expect(process).toHaveBeenCalledWith(input);
+      expect(result).toEqual(outputs.shift());
+    });
+  });
 });
