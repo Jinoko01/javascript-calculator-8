43219d92841acf3e3d7b0cfba0ed845d9cbd7cff
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 16:00:38 +0900

    test(parser): Add parser test case
    setDelimiter, split method

---
+++ __tests__/ParserTest.js
@@ -0,0 +1,33 @@
+import Parser from '../src/entities/Parser.js';
+
+describe('Parser 테스트', () => {
+  it('setDelimiter 메소드를 통해 기본 및 커스텀 구분자 정보를 반환한다.', () => {
+    const inputs = ['1,2,3', '//;\n1;2;3', '//@\n1@2@3'];
+    const returns = [
+      [',', ':'],
+      [',', ':', ';'],
+      [',', ':', '@'],
+    ];
+
+    const parser = new Parser();
+
+    inputs.forEach((input, index) => {
+      expect(parser.setDelimiter(input)).toEqual(returns[index]);
+    });
+  });
+
+  it('split 메소드는 구분자 정보를 기반으로 문자열을 나눠 숫자 배열을 반환한다.', () => {
+    const inputs = ['1,2,3', '//;\n1;2;3', '//@\n1@2@3'];
+    const returns = [
+      [1, 2, 3],
+      [1, 2, 3],
+      [1, 2, 3],
+    ];
+
+    const parser = new Parser();
+
+    inputs.forEach((input, index) => {
+      expect(parser.split(input)).toEqual(returns[index]);
+    });
+  });
+});
