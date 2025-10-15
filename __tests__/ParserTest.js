9c351b808fd7542439e12c64ee5b9df0e24746a3
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 16:18:28 +0900

    test(parser): Add parser test case
    parse method

--- __tests__/ParserTest.js
+++ __tests__/ParserTest.js
@@ -1,6 +1,21 @@
 import Parser from '../src/entities/Parser.js';
 
 describe('Parser 테스트', () => {
+  it('기본 및 커스텀 구분자를 통해 나눠진 숫자 배열을 반환한다.', () => {
+    const inputs = ['1,2,3', '//;\n1;2;3', '//@\n1@2,3:4'];
+    const returns = [
+      [1, 2, 3],
+      [1, 2, 3],
+      [1, 2, 3, 4],
+    ];
+
+    const parser = new Parser();
+
+    inputs.forEach((input, index) => {
+      expect(parser.parse(input)).toEqual(returns[index]);
+    });
+  });
+
   it('setDelimiter 메소드를 통해 기본 및 커스텀 구분자 정보를 반환한다.', () => {
     const inputs = ['1,2,3', '//;\n1;2;3', '//@\n1@2@3'];
     const returns = [
