325b40bab56717f77487d01ab33ea749ab3b22a0
dswvgw6412 <dswvgw6412@naver.com>
2025-10-15 16:20:07 +0900

    test(splitter): Add splitter test case

---
+++ __tests__/SplitterTest.js
@@ -0,0 +1,22 @@
+import Splitter from '../src/entities/Splitter.js';
+
+describe('Splitter 테스트', () => {
+  it('split 메소드는 구분자를 기반으로 나누어진 숫자 배열을 반환한다.', () => {
+    const inputs = [
+      ['1,2,3', [',', ':']],
+      ['1;2;3', [',', ':', ';']],
+      ['1@2@3', [',', ':', '@']],
+    ];
+    const returns = [
+      [1, 2, 3],
+      [1, 2, 3],
+      [1, 2, 3],
+    ];
+
+    const splitter = new Splitter();
+
+    inputs.forEach(([input, delimiter], index) => {
+      expect(splitter.split(input, delimiter)).toEqual(returns[index]);
+    });
+  });
+});
