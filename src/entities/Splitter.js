5e8eedf4371f80c47e0caf43695dbbc333967f5f
dswvgw6412 <dswvgw6412@naver.com>
2025-10-16 17:39:09 +0900

    fix(splitter): Fix custom delimiter validation

--- src/entities/Splitter.js
+++ src/entities/Splitter.js
@@ -1,9 +1,20 @@
+import { ERROR_MESSAGE } from '../data/messages.js';
+import { REGEX } from '../data/regex.js';
+
 export default class Splitter {
   split(input, delimiter) {
-    const escaped = delimiter
-      .map((s) => s.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&'))
-      .sort((a, b) => b.length - a.length);
+    const escaped = delimiter.map((s) =>
+      s.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&')
+    );
     const delimiterRegex = new RegExp(`(?:${escaped.join('|')})`, 'g');
-    return input.split(delimiterRegex).map(Number);
+    const result = input.split(delimiterRegex);
+
+    result.forEach((num) => {
+      if (!REGEX.NUMBER.test(num)) {
+        throw new Error(ERROR_MESSAGE.INVALID_NUMBER);
+      }
+    });
+
+    return result.map(Number);
   }
 }
