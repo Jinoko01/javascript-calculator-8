a991157c36cfaf7dd5c87ed2afe54477b22e4b76
dswvgw6412 <dswvgw6412@naver.com>
2025-10-16 16:53:11 +0900

    test(inputProcessor): Remove unnecessary test case

--- __tests__/InputProcessorTest.js
+++ __tests__/InputProcessorTest.js
@@ -13,11 +13,6 @@ describe('InputProcessor 테스트', () => {
     });
   });
 
-  it('빈 문자열을 입력할 경우 에러를 발생시킨다.', () => {
-    const inputProcessor = new InputProcessor();
-    expect(() => inputProcessor.processInput('')).toThrow();
-  });
-
   it('문자열이 아닌 경우 에러를 발생시킨다.', () => {
     const inputs = [123, true, null, undefined, {}, []];
     const inputProcessor = new InputProcessor();
