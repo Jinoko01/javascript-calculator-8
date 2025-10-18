bae6be7769a3f085414d8f13af282354bffc734a
dswvgw6412 <dswvgw6412@naver.com>
2025-10-18 21:38:21 +0900

    refactor(splitter): Add validateNumber method

--- src/entities/Splitter.js
+++ src/entities/Splitter.js
@@ -1,15 +1,12 @@
 import { ERROR_MESSAGE } from '../data/messages.js';
+import { REGEX } from '../data/regex.js';
 
 export default class Splitter {
   split(input, delimiter) {
     const delimiterRegex = this.getEscapedDelimiters(delimiter);
     const result = input.split(delimiterRegex);
 
-    result.forEach((num) => {
-      if (Number.isNaN(Number(num)) || num === '') {
-        throw new Error(ERROR_MESSAGE.INVALID_NUMBER);
-      }
-    });
+    this.validateNumber(result);
 
     return result.map(Number);
   }
@@ -19,5 +16,13 @@ export default class Splitter {
       s.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&')
     );
     return new RegExp(`(?:${escaped.join('|')})`, 'g');
+  }
+
+  validateNumber(result) {
+    result.forEach((num) => {
+      if (!REGEX.NUMBER.test(num)) {
+        throw new Error(ERROR_MESSAGE.INVALID_NUMBER);
+      }
+    });
   }
 }
